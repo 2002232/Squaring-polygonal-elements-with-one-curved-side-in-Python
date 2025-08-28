73e07b734270a80024829697b52850fdfa73c34a
2002232 <giovanni.traversin@studenti.unipd.it>
2025-08-28 13:46:04 +0200

    Update trigauss.py

--- trigauss.py
+++ trigauss.py
@@ -71,69 +71,127 @@ def trigauss(n, alpha, beta):
   # angles and weights for the trigonometric gaussian formula
   return  np.column_stack((2*np.arcsin(np.sin(omega/2)*xw[:,0]) + \
    (beta+alpha)/2, xw[:,1]))
-
+  
 #-------------------------------------------------------------------------------
 
 def chebyshev(N, Mom, Abm=False):
+
+#  Modified Chebyshev algorithm.
+
+#  Given a weight function w encoded by its first 2n modified
+#  moments, stored in the (row) vector mom, relative to monic
+#  polynomials defined by the (2n-1)x2 array abm of their
+#  recurrence coefficients, [ab,normsq]=CHEBYSHEV(n,mom,abm)
+#  generates the array ab of the first n recurrence coefficients
+#  of the orthogonal polynomials for the weight function w, and
+#  the vector normsq of their squared norms. The n alpha-
+#  coefficients are stored in the first column, the n beta-
+#  coefficients in the second column, of the nx2 array ab. The
+#  call [ab,normsq]=CHEBYSHEV(n,mom) does the same, but using the
+#  classical Chebyshev algorithm. If n is larger than the sizes
+#  of mom and abm warrant, then n is reduced accordingly.
+
   mom = np.array(Mom)
   normsq = []
   if N <= 0:
      raise ('N out of range')
   if N > len(mom)/2:
     N = len(mom)/2
+
   if type(Abm) == bool:
     abm = np.zeros((2,int(2*N-1)))
   else:
     abm = np.array(Abm)
   if len(abm[0]) == 2 and len(abm)!=2:
       abm = abm.T
+
   if N > (len(abm[0])+1)/2:
     N = (len(abm[0])+1)/2
+
   if N == 1:
     normsq = [mom[0]]
+
   N = int(N)
   ab =  np.array([[abm[0][0]+mom[1]/mom[0], mom[0]]]+[[0,0]]*(N-1))
   sig = np.zeros((N+1,2*N))
   sig[1] = mom[0:int(2*N)]
+
+  #NOTE: abm is a matrix with two raws instead of a matrix with two
+  # columns, therefore the indexes are reversed.
+
   for n in range(2,N+1):
     for m in range(n-1,(2*N-n+1)):
-      sig[n][m] = sig[n-1][m+1]-(ab[n-2][0]-abm[0][m])*sig[n-1][m]-\
-      ab[n-2][1]*sig[n-2][m]+abm[1][m]*sig[n-1][m-1]
-    ab[n-1,0] = abm[0][n-1] + sig[n][n]/sig[n][n-1]-\
-    sig[n-1][n-1]/sig[n-1][n-2]
+      sig[n][m] = sig[n-1][m+1]-(ab[n-2][0]-abm[0][m])*sig[n-1][m]-ab[n-2][1]*sig[n-2][m]+abm[1][m]*sig[n-1][m-1]
+    ab[n-1,0] = abm[0][n-1] + sig[n][n]/sig[n][n-1] - sig[n-1][n-1]/sig[n-1][n-2]
     ab[n-1,1] = sig[n][n-1]/sig[n-1][n-2]
+
   for i in range(N):
     normsq.append(sig[i+1][i])
+
+
   return ab, np.array(normsq)
 
 #-------------------------------------------------------------------------------
 
 def tridisolve(a,b,c,d):
+
+  #   TRIDISOLVE  Solve tridiagonal system of equations.
+  # From Cleve Moler's Matlab suite
+  # http://www.mathworks.it/moler/ncmfilelist.html
+  #     x = TRIDISOLVE(a,b,c,d) solves the system of linear equations
+  #     b(1)*x(1) + c(1)*x(2) = d(1),
+  #     a(j-1)*x(j-1) + b(j)*x(j) + c(j)*x(j+1) = d(j), j = 2:n-1,
+  #     a(n-1)*x(n-1) + b(n)*x(n) = d(n).
+  #
+  #   The algorithm does not use pivoting, so the results might
+  #   be inaccurate if abs(b) is much smaller than abs(a)+abs(c).
+  #   More robust, but slower, alternatives with pivoting are:
+  #     x = T\d where T = diag(a,-1) + diag(b,0) + diag(c,1)
+  #     x = S\d where S = spdiags([[a; 0] b [0; c]],[-1 0 1],n,n)
+
   x = d
   n = len(x)-1
   for j in range(0,n):
     mu = a[j]/b[j]
     b[j+1] = b[j+1] - mu*c[j]
     x[j+1] = x[j+1] - mu*x[j]
+
   x[n] = x[n]/b[n]
   for i in range(n)[::-1]:
     x[i] = (x[i]-c[i]*x[i+1])/b[i]
+
   return x
 
 #-------------------------------------------------------------------------------
 
 def gauss(N, albet):
+
+#    Gauss quadrature rule.
+
+#    Given a weight function w encoded by the nx2 array albet of the
+#    first n recurrence coefficients for the associated orthogonal
+#    polynomials, the first column of ab containing the n alpha-
+#    coefficients and the second column the n beta-coefficients,
+#    the call xw=GAUSS(n,ab) generates the nodes and weights xw of
+#    the n-point Gauss quadrature rule for the weight function w.
+#    The nodes, in increasing order, are stored in the first
+#    column, the n corresponding weights in the second column, of
+#    the nx2 array xw.
+
   ab = np.array(albet)
   if len(ab[:,0]) < N:
       raise ValueError('Input array ab is too short')
   J = np.zeros((N, N))
+
   for i in range(N):
       J[i][i] = ab[i][0]
   for j in range(N-1):
       J[j+1][j] = np.sqrt(ab[j+1][1])
       J[j][j+1] = J[j+1][j]
+
   D, V = np.linalg.eigh(J)
   I = np.argsort(D)
   D = np.sort(D)
   V = V[:,I]
+
   return np.transpose((D , ab[0][1]*V[0,:]**2))
