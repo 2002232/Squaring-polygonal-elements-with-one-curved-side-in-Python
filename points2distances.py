9d58980fa30fd89c1076020921eb2d0fe755980c
2002232 <giovanni.traversin@studenti.unipd.it>
2025-08-28 19:22:29 +0200

    points2distances.py

---
+++ points2distances.py
@@ -0,0 +1,15 @@
+def points2distances(p):
+
+  # Create euclidean distance matrix from point matrix.
+  points = np.array(p)
+
+  # All inner products between points.
+  distances = points @ np.transpose(points)
+
+  # Vector of squares of norms of points.
+  lsq = np.diag(distances)
+
+  a = np.array([[i]*len(points) for i in lsq])
+
+  # distances = np.sqrt(a+np.transpose(a)-2*distances)
+  return np.sqrt(a+np.transpose(a)-2*distances)
